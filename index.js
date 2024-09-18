831d9f1e0055410d0e654b85a05b3cc3624e20cb
Gevendra Sahu <123631888+gevendra2004@users.noreply.github.com>
2024-09-18 16:04:04 +0530

    image-slider-starter

---
+++ index.js
@@ -0,0 +1,6 @@
+let items = document.querySelectorAll(".item");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+let thumbnails = document.querySelectorAll(".slide");
+
+//config param
